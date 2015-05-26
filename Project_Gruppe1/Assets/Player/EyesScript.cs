62f19cd65c5177485061c541bf91c229ca1c11f4
jensfa <jensffb@gmail.com>
2015-05-26 14:09:57 +0200

    Augen schließen Test hinzugefügt

---
+++ Project_Gruppe1/Assets/Player/EyesScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyesScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Mouse1)) {
+			GameObject.FindGameObjectWithTag("EyesOverlay").GetComponent<Animator>().SetTrigger("CloseEyes");
+		}
+		if (Input.GetKeyUp (KeyCode.Mouse1)) {
+			GameObject.FindGameObjectWithTag("EyesOverlay").GetComponent<Animator>().SetTrigger("OpenEyes");
+		}
+	}
+}
