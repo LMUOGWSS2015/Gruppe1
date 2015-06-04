9d16081c9fe4d4c2585241d28e757fdfe516938e
guerrero4de <guerrero4de@yahoo.de>
2015-06-05 00:12:11 +0200

    Piano plays mistreiously

---
+++ Project_Gruppe1/Assets/Soundeffekte/Musik/piano.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var pianoClip:AudioClip;
+var isPlaying = false;
+
+function OnTriggerEnter(o:Collider){
+	if(isPlaying == false){
+		GetComponent.<AudioSource>().Play();
+	}
+	isPlaying = true;
+}
+
+function OnTriggerExit(o:Collider){
+	if(isPlaying == true){
+		GetComponent.<AudioSource>().Pause();
+	}
+	isPlaying = false;
+}
