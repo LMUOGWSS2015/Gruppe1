5b0fde4240696afeac92e6484af5c102d46ea08c
jensfa <jensffb@gmail.com>
2015-06-15 12:45:18 +0200

    Monster Test Scene

---
+++ Project_Gruppe1/Assets/Monster/MonsterAuftritt1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterAuftritt1 : MonoBehaviour {
+
+	Vector3 goal;
+	public float speed;
+	GameObject monster;
+
+	// Use this for initialization
+	void Start () {
+		//goal = GameObject.FindGameObjectWithTag ("Goal").transform.position;
+		monster = GameObject.FindGameObjectWithTag ("Monster");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		goal = GameObject.FindGameObjectWithTag ("Player").transform.position;
+		// The step size is equal to speed times frame time.
+		float step = monster.GetComponent<MonsterScipt>().speed * Time.deltaTime;
+		monster.transform.position = Vector3.MoveTowards(monster.transform.position, new Vector3(goal.x, monster.transform.position.y, goal.z), step);
+		monster.transform.LookAt(goal + new Vector3(0,-2.1f,0));
+	}
+}
---
+++ Project_Gruppe1/Assets/Monster/MonsterScipt.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterScipt : MonoBehaviour {
+
+	public float speed = 0;
+	float wantedspeed = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (speed < wantedspeed) {
+			speed +=0.35f;
+		} else if (speed > wantedspeed){
+			speed -=0.35f;
+		}
+	}
+
+	void Slower() {
+		wantedspeed = 0.35f;
+		speed = wantedspeed;
+		//speed = wantedspeed;
+	}
+
+	void Faster() {
+		wantedspeed = 0.35f * 10f;
+		//wantedspeed = speed;
+	}
+
+}
