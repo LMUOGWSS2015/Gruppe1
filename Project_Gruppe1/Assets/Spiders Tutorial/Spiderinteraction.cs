c5e1080b7688747ccdd4875e0cd08717d2998a4f
guerrero4de <guerrero4de@yahoo.de>
2015-06-16 16:15:55 +0200

    SpiderInteraction

---
+++ Project_Gruppe1/Assets/Spiders Tutorial/Spiderinteraction.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * Script for FPSController
+ */
+public class SpiderInteraction : MonoBehaviour {
+
+	private EyesScript es;
+
+	// Use this for initialization
+	void Start () 
+	{
+		es  = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<EyesScript>();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		// Game object need tag "Monster"
+		if (other.gameObject.CompareTag ("Spiders")) 
+		{
+			Debug.Log ("Ahhhh Spider!");
+			StartCoroutine(SpiderDie(3));
+		}
+	}
+
+	IEnumerator SpiderDie(int x) {
+		Debug.Log ("Oh nein, ich sterbe... In " + x + " Sekunden bin ich tot, wenn ich nicht die Augen schliesse.");
+		yield return new WaitForSeconds(x);
+		if (!es.getEyesClosed ()) {
+			GameObject.FindGameObjectWithTag ("Spiders").GetComponent<Animator> ().SetTrigger ("SpiderMove");
+			//TODO SpiderMove Animation
+		} else {
+			GameObject.FindGameObjectWithTag("Spiders").SetActive(false);
+			Debug.Log ("============================== Gefahr vorüber! Augen können geöffnet werden.");
+		}
+	}
+}
