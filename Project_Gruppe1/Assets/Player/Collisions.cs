7d58bea7c465a828bccbe207fb14d8e5f2629b94
bened63 <bened63@github.com>
2015-06-17 12:10:44 +0200

    Versteck prefab hinzugefügt, Collisions script update

--- Project_Gruppe1/Assets/Player/Collisions.cs
+++ Project_Gruppe1/Assets/Player/Collisions.cs
@@ -7,20 +7,22 @@ public class Collisions : MonoBehaviour {
 	private EyesScript es;
 
 	// Hide spot
-	private bool openHideSpot;
+	private bool hideSpotOpen;
 	private GameObject go;
 	private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-
+		Debug.Log ("#################Start Collisions Script");
+		
 		// Monster: Get eye script
 		es  = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<EyesScript>();
 
 		// Hide spot
-		openHideSpot = false;
+		hideSpotOpen = false;
 		go = GameObject.Find("/Grundriss/Erdgeschoss/Versteck/hide_spot_2/door");
 		rb = go.GetComponent<Rigidbody>();
+		rb.isKinematic = true;
 		rb.useGravity = false;
 
 	}
@@ -30,24 +32,20 @@ public class Collisions : MonoBehaviour {
 
 	}
 
-	
-	void OnTriggerEnter(Collider other) 
-	{
+	// Trigger enter: check tag of entered (collided) game object
+	void OnTriggerEnter(Collider other) {
+
 		// Monster
-		if (other.gameObject.CompareTag ("Monster")) 
-		{
+		if (other.gameObject.CompareTag ("Monster")) {
 			Debug.Log ("Ahhhh Monster!");
 			StartCoroutine(Die(3));
 		}
 
-		// Hide spot
-		if (other.gameObject.CompareTag ("HideSpot")) 
-		{
-			// Hide spot: OPEN
-			openHideSpot = true;
-			go.transform.rotation = Quaternion.Euler(0, 0, -0.5f);
-			rb.isKinematic = false;
-			rb.useGravity = true;
+		// Open hide spot
+		if (other.gameObject.CompareTag ("HideSpot")) {
+			if (!hideSpotOpen) {
+				OpenHideSpot();
+			}
 		}
 	}
 
@@ -57,10 +55,19 @@ public class Collisions : MonoBehaviour {
 	IEnumerator Die(int x) {
 		Debug.Log ("Oh nein, ich sterbe... In " + x + " Sekunden bin ich tot, wenn ich nicht die Augen schliesse.");
 		yield return new WaitForSeconds(x);
-		if (!es.getEyesClosed ()) 
-		{
+		if (!es.getEyesClosed ()) {
 			GameObject.FindGameObjectWithTag("Player").GetComponentInParent<Animator>().SetTrigger("die");
 		}
 		Debug.Log ("============================== Gefahr vorüber! Augen können geöffnet werden.");
+	}
+
+	/*
+	 * Open hide spot
+	 */
+	void OpenHideSpot() {
+		hideSpotOpen = true;
+		go.transform.rotation = Quaternion.Euler(0, 0, -0.5f);
+		rb.isKinematic = false;
+		rb.useGravity = true;
 	}
 }
