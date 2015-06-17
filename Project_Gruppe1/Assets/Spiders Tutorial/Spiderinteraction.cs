f010378abc62040367c8e443d08211fe0070135b
guerrero4de <guerrero4de@yahoo.de>
2015-06-17 20:02:38 +0200

    SpiderTutorial

--- Project_Gruppe1/Assets/Spiders Tutorial/Spiderinteraction.cs
+++ Project_Gruppe1/Assets/Spiders Tutorial/Spiderinteraction.cs
@@ -10,60 +10,48 @@ public class Spiderinteraction : MonoBehaviour {
 
 	private EyesScript es;
 	private string subtitle;
-	public Transform target;
-	Quaternion targetRotation; // change it whenever you need to turn
-	float turnSpeed = 0f; // internal property
-	float turnSpeedChange = 20f; // acceleration of turning
-	Transform rotatingTransform; // transform to rotate
+	private GameObject player;
+	private Text subtitles;
 
 	// Use this for initialization
 	void Start () 
 	{
 		es  = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<EyesScript>();
+		player = GameObject.FindGameObjectWithTag ("Player");
+		subtitles = GameObject.FindGameObjectWithTag ("Subtitles").GetComponent<Text>();
 	}
 
-	void OnTriggerEnter(Collider other) 
-	{
-		if (other.gameObject.CompareTag ("SpiderCollider")) 
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("TutorialCollider")) 
 		{
-			GameObject.FindGameObjectWithTag ("Player").GetComponent<FirstPersonController>().enabled = false;
-			GameObject.FindGameObjectWithTag ("Player").GetComponent<CharacterController> ().enabled = false;
+			GameObject.FindGameObjectWithTag ("TutorialCollider").SetActive(false);
+			player.GetComponent<FirstPersonController>().enabled = false;
+			player.GetComponent<CharacterController>().enabled = false;
 
-			GameObject.FindGameObjectWithTag ("Player").transform.LookAt(target);
-			//Quaternion rotation = Quaternion.LookRotation (GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform>().position);   
-			//GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform>().rotation = Quaternion.Slerp (GameObject.FindGameObjectWithTag ("Player").GetComponent<Transform>().rotation, rotation, Time.deltaTime);
+			player.GetComponent<SmoothLookAt>().target = GameObject.FindGameObjectWithTag("Spider").GetComponent<Transform>();
+			player.GetComponent<SmoothLookAt>().enabled = true;
 
-			/*//angle we need to turn
-			float angleToTurn = Quaternion.Angle(rotatingTransform.rotation, targetRotation);
-			//speed is in degrees/sec = angle, to pass angle in 1 seconds. our speed can be increased only 'turnSpeedChange' degrees/sec^2, but don't increase it if needn't
-			turnSpeed = Mathf.Min(angleToTurn, turnSpeed + turnSpeedChange * Time.fixedDeltaTime);
-			//rotate
-			rotatingTransform.rotation = Quaternion.Lerp(rotatingTransform.rotation, targetRotation, Mathf.Clamp01(angleToTurn > 0 ? turnSpeed * Time.fixedDeltaTime / angleToTurn : 0f));
-			*/
-
-			GameObject.FindGameObjectWithTag ("SpiderCollider").SetActive (false);
-			GameObject.FindGameObjectWithTag ("Subtitles").GetComponent<Text>().text = "Close your Eyes";
-			StartCoroutine(Spiderattack(3));
+			subtitles.text = "See the giant spider? Close your Eyes!";
+			GameObject.FindGameObjectWithTag("Tutorial").GetComponent<Animator>().SetTrigger("TutorialTrigger");
+			StartCoroutine(Spiderattack(10));
 		}
 	}
 
 	IEnumerator Spiderattack(int x) {
 		yield return new WaitForSeconds(x);
 		if (!es.getEyesClosed ()) {
-			GameObject.FindGameObjectWithTag ("Subtitles").GetComponent<Text>().text = "You took too long.";		
-			} else {
-				GameObject.FindGameObjectWithTag("Spiders").SetActive(false);
-			GameObject.FindGameObjectWithTag ("Subtitles").GetComponent<Text>().text = "See? It was only your imagination.";	
-			}
-		yield return new WaitForSeconds(5);
-		GameObject.FindGameObjectWithTag ("Subtitles").GetComponent<Text>().text = "";	
-		GameObject.FindGameObjectWithTag ("Player").GetComponent<FirstPersonController>().enabled = true;
-		GameObject.FindGameObjectWithTag ("Player").GetComponent<CharacterController> ().enabled = true;
+			subtitles.text = "You took too long.";	
+			//TODO what else happens here?
+		} else {
+			GameObject.FindGameObjectWithTag("Tutorial").SetActive(false);
+			subtitles.text = "Open your Eyes again. See? It was only your imagination.";	
 		}
 
-	void Update() {
-		// Rotate the camera every frame so it keeps looking at the target 
-		//GameObject.FindGameObjectWithTag ("Player").transform.LookAt(target);
+		player.GetComponent<SmoothLookAt>().enabled = false;
+		player.GetComponent<FirstPersonController>().enabled = true;
+		player.GetComponent<CharacterController>().enabled = true;
+		yield return new WaitForSeconds(5);
+		subtitles.text = "";	
 	}
 }
 
