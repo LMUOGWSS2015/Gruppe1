95e39ea07958d0b0fcc9c80f8f52252993a182a8
XaverLoeffelholz <xaver@loeffelholz.de>
2015-06-28 22:31:39 +0200

    Revert "Eye Tracking added"
    This reverts commit d9e1d0bd875cab3b18652771ca497c42be98fdfb.

---
+++ Project_Gruppe1/Assets/Plugins/MyScripts/MyTestScriptForGaze.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using iView;
+
+public class MyTestScriptForGaze : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("MyTestScriptForGaze START");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//get the Sample from the Server
+		SampleData sample = SMIGazeController.Instance.GetSample();
+		
+		//get the averaged GazePosition
+		Vector3 averageGazePosition = sample.averagedEye.gazePosInUnityScreenCoords (); 
+
+	}
+}
