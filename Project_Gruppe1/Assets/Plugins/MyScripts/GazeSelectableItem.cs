7db659acf30831362f5e9ea407af89842e84b125
bened63 <bened63@github.com>
2015-06-30 16:02:11 +0200

    EyeTracking Scripts

---
+++ Project_Gruppe1/Assets/Plugins/MyScripts/GazeSelectableItem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeSelectableItem : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void OnGazeEnter() {
+		Debug.Log ("++++++++++++++++++++++++++++++++ OnGazeEnter()");
+		GetComponent<Renderer>().enabled = true; // only for testing important
+		GetComponent<Renderer>().material.color = Color.blue;
+	}
+	
+	public void OnGazeExit() {
+		Debug.Log ("------------------------------------------------------------------------ OnGazeExit()");
+		GetComponent<Renderer>().material.color = Color.black;
+		CancelInvoke("WaitForMissingsample");
+		StartCoroutine(WaitForMissingsample());
+	}
+	
+	IEnumerator WaitForMissingsample() {
+		yield return new WaitForSeconds(0.25f);
+		//		scaleDestination = startScale;
+		
+	}
+}
