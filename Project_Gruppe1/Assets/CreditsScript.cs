af099448d7e63f8df09347b96b1c26d2cc1aa69b
roppelt <bianka.roppelt@googlemail.com>
2015-07-07 19:56:25 +0200

    Hinweise und Credits
    Hinweise verschwinden nach aufheben; Hinweise faden aus;

--- Project_Gruppe1/Assets/CreditsScript.cs
+++ Project_Gruppe1/Assets/CreditsScript.cs
@@ -7,6 +7,10 @@ public class CreditsScript : MonoBehaviour {
 	public Texture2D creditsBackgroundImage;
 	public GameObject creditsMusic;
 	
+	public float fadeSpeed = 0.2f;
+	private float alpha = 0f;
+	private int fadeDir = 1;
+	
 	private float startTime = 0.1f;
 	
 	private float savedTimeScale;
@@ -18,14 +22,24 @@ public class CreditsScript : MonoBehaviour {
 	
 	private int foundHints = 0;
 	private AudioSource creditsAudio;
-
 	
+	private bool showNames = true;
+	private bool hideNames = false;
+
+
 	void Start() {
 		guiFactor = (int) Mathf.Floor (Screen.width/1024);
 		guiFactor = (guiFactor == 0) ? 1 : guiFactor;
 		
 		creditsAudio = creditsMusic.GetComponent<AudioSource> ();
 
+	}
+	
+	void LateUpdate() {
+		// TODO: remove; only for testing
+		if (getCountHints() == 2) {
+			startCredits();
+		}
 	}
 	
 	int getCountHints() {
@@ -65,10 +79,8 @@ public class CreditsScript : MonoBehaviour {
 			Cursor.visible = true;
 		}
 	}
-
-
-
 	
+
 	void OnGUI () {
 		if (skin != null) {
 			GUI.skin = skin;
@@ -77,60 +89,73 @@ public class CreditsScript : MonoBehaviour {
 			creditsGUI();
 		} 
 	}
-
+	
 	void creditsGUI() {
 		GUI.DrawTexture (new Rect(0,0,Screen.width , Screen.height),creditsBackgroundImage);
 		
-		GUIStyle titleStyle = new GUIStyle (GUI.skin.label);
-		titleStyle.alignment = TextAnchor.MiddleCenter;
-		titleStyle.fontSize  *= guiFactor;
-
-		GUI.Label (new Rect (0, 0, Screen.width, 200*guiFactor), "DON'T LOOK AT ME!" , titleStyle);
-
-		ArrayList array = new ArrayList ();
-		array.Add ("Gruppenmitglieder:");
-		array.Add ("Xaver Loeffelholz");
-		array.Add ("Jens Fakesch");
-		array.Add ("Lüppi Orerreug");
-		array.Add ("Benjamin Eder");
-		array.Add ("Mai Anh Nguyen");
-		array.Add ("Inga Brehm");
-		array.Add ("Bianka Roppelt");
-		
-		
-		GUIStyle textStyle = new GUIStyle (GUI.skin.label);
-		textStyle.alignment = TextAnchor.MiddleCenter;
-		textStyle.fontSize  = Mathf.FloorToInt(Mathf.Floor(textStyle.fontSize * guiFactor * 0.5f));
-		
-		float height = 70*guiFactor;
-		
-		for(var i=0;i<array.Count;i++)
-		{
-			GUI.Label(new Rect(0,200+(i*height),Screen.width,height),(string) array[i], textStyle);
+		if (!hideNames) {
+			alpha += fadeDir * fadeSpeed * Time.deltaTime;	
+			alpha = Mathf.Clamp01 (alpha);	
+			
+			Color thisColor = GUI.color;
+			thisColor.a = alpha;
+			GUI.color = thisColor;
+			
+			
+			GUIStyle titleStyle = new GUIStyle (GUI.skin.label);
+			titleStyle.alignment = TextAnchor.MiddleCenter;
+			titleStyle.fontSize *= guiFactor;
+			
+			GUI.Label (new Rect (0, 0, Screen.width, 200 * guiFactor), "DON'T LOOK AT ME!", titleStyle);
+			
+			
+			
+			GUIStyle textStyleContent = new GUIStyle (GUI.skin.label);
+			textStyleContent.alignment = TextAnchor.MiddleCenter;
+			textStyleContent.fontSize = Mathf.FloorToInt (Mathf.Floor (textStyleContent.fontSize * guiFactor * 0.5f));
+			
+			GUIStyle textStyleTitle = new GUIStyle (GUI.skin.label);
+			textStyleTitle.alignment = TextAnchor.MiddleCenter;
+			textStyleTitle.fontSize = Mathf.FloorToInt (Mathf.Floor (textStyleTitle.fontSize * guiFactor * 0.7f));
+			
+			float heightContent = 50 * guiFactor;
+			float heightTitle = 90 * guiFactor;
+			
+			
+			ArrayList mitgliederArray = new ArrayList ();
+			mitgliederArray.Add ("Xaver Loeffelholz");
+			mitgliederArray.Add ("Jens Fakesch");
+			mitgliederArray.Add ("Christian Guerrero");
+			mitgliederArray.Add ("Benjamin Eder");
+			mitgliederArray.Add ("Mai-Anh Nguyen");
+			mitgliederArray.Add ("Inga Brehm");
+			mitgliederArray.Add ("Bianka Roppelt");
+			
+			GUI.Label (new Rect (0, 200 * guiFactor, Screen.width, heightTitle), "Gruppenmitglieder:", textStyleTitle);
+			for (var i=0; i<mitgliederArray.Count; i++) {
+				GUI.Label (new Rect (0, 200 * guiFactor + heightTitle + (i * heightContent), Screen.width, heightContent), (string)mitgliederArray [i], textStyleContent);
+			}
 		}
-//		GUILayout.BeginArea( new Rect(0, 0, Screen.width, Screen.height * 2));
-//
-//		GUI.DrawTexture (new Rect(0,0,Screen.width , Screen.height),creditsBackgroundImage);
-//
-//		GUIStyle titleStyle = new GUIStyle (GUI.skin.label);
-//		titleStyle.alignment = TextAnchor.MiddleCenter;
-//		titleStyle.fontSize  *= guiFactor;
-//		
-//		GUIStyle textStyle = new GUIStyle (GUI.skin.label);
-//		textStyle.alignment = TextAnchor.MiddleCenter;
-//		textStyle.fontSize  = textStyle * guiFactor * 0.5;
-//
-//		GUI.Label (new Rect (0, 0, Screen.width, 200), "DON'T LOOK AT ME!" , titleStyle);
-//		GUI.Label (new Rect (0, 200, Screen.width, 100), "Gruppenmitglieder:" , textStyle);
-//		GUI.Label (new Rect (0, 300, Screen.width, 100), "Xaver Loeffelholz" , textStyle);
-//		GUI.Label (new Rect (0, 400, Screen.width, 100), "Jens Fakesch" , textStyle);
-//		GUI.Label (new Rect (0, 500, Screen.width, 100), "Lüppi Orerreug" , textStyle);
-//		GUI.Label (new Rect (0, 600, Screen.width, 100), "Benjamin Eder" , textStyle);
-//		GUI.Label (new Rect (0, 700, Screen.width, 100), "Mai Anh Nguyen" , textStyle);
-//		GUI.Label (new Rect (0, 800, Screen.width, 100), "Inga Brehm" , textStyle);
-//		GUI.Label (new Rect (0, 900, Screen.width, 100), "Bianka Roppelt" , textStyle);
-//
-//
-//		GUILayout.EndArea();
+		if (showNames) {
+			Invoke ("hideGUINames", 3.0f);
+		}
+		else if (!showNames && !(hideNames)) {
+			Invoke ("hiddenGUINames", 3.0f);
+		}
+	}
+
+	void showGUINames() {
+		fadeDir = 1;
+		hideNames = false;
+		showNames = true;		
+	}
+	void hideGUINames() {
+		fadeDir = -1;
+		hideNames = false;
+		showNames = false;		
+	}
+	void hiddenGUINames() {
+		hideNames = true;	
+		Application.LoadLevel (0);
 	}
 }
--- Project_Gruppe1/Assets/Player/InteractionScript.cs
+++ Project_Gruppe1/Assets/Player/InteractionScript.cs
@@ -20,6 +20,7 @@ public class InteractionScript : MonoBehaviour {
 	public Texture hint3;
 	public Texture hint4;
 	private bool showGUIOverlay = false;
+	private bool hideGUIOverlay = true;
 	
 	public bool gotKey = false;
 	public bool gotFlashlight = false;
@@ -219,11 +220,15 @@ public class InteractionScript : MonoBehaviour {
 				} else if (hit.collider.CompareTag ("Hint")) {
 					Debug.Log ("Hint gefunden");
 					foundHints++;
+					Renderer[] renderers = hit.transform.parent.GetComponentsInChildren<Renderer>();
+					foreach(Renderer r in renderers) {
+						r.enabled = false;
+					}
 					Debug.Log ("Hinweise nr: " + foundHints);
 					if (numberOfHints == foundHints) {
 						Debug.Log ("Alle Hinweise da");	
 					}	
-					showGUIOverlay = true;		
+					showGUI();	
 				} else if (hit.collider.CompareTag ("Key")) {
 					gotKey = true;
 					Debug.Log ("Schlüßel gefunden");
@@ -259,39 +264,52 @@ public class InteractionScript : MonoBehaviour {
 	
 	
 	void OnGUI() {     
-		if (showGUIOverlay) {
-			Texture hint = gray_overlay;
-			switch(foundHints) {
-			case 1: 
-				hint = hint1;
-				break;
-			case 2: 
-				hint = hint2;
-				break;
-			case 3: 
-				hint = hint3;
-				break;
-			case 4: 
-				hint = hint4;
-				break;
-			}
-			
+		Texture hint = gray_overlay;
+		switch (foundHints) {
+		case 1: 
+			hint = hint1;
+			break;
+		case 2: 
+			hint = hint2;
+			break;
+		case 3: 
+			hint = hint3;
+			break;
+		case 4: 
+			hint = hint4;
+			break;
+		}
+		if (!hideGUIOverlay) {
 			alpha += fadeDir * fadeSpeed * Time.deltaTime;	
-			alpha = Mathf.Clamp01(alpha);	
+			alpha = Mathf.Clamp01 (alpha);	
 			
 			Color thisColor = GUI.color;
 			thisColor.a = alpha;
 			GUI.color = thisColor;
 			
-			GUI.DrawTexture(new Rect(0f, 0f, Screen.width, Screen.height), gray_overlay);
+			GUI.DrawTexture (new Rect (0f, 0f, Screen.width, Screen.height), gray_overlay);
 			GUI.DrawTexture (new Rect (Screen.width / 2 - 200, Screen.height / 2 - 200, 400, 400), hint); 
 			
-			Invoke("hideGUI", 3.0f);
+			if (showGUIOverlay) {
+				Invoke ("hideGUI", 3.0f);
+			} else if (!showGUIOverlay && !(hideGUIOverlay)) {
+				Invoke ("hiddenGUI", 3.0f);
+			}
 		}
 	}
-	
+
+	void showGUI() {
+		fadeDir = 1;
+		hideGUIOverlay = false;
+		showGUIOverlay = true;		
+	}
 	void hideGUI() {
+		fadeDir = -1;
+		hideGUIOverlay = false;
 		showGUIOverlay = false;		
+	}
+	void hiddenGUI() {
+		hideGUIOverlay = true;		
 	}
 
 	public void PlayerDies(){
