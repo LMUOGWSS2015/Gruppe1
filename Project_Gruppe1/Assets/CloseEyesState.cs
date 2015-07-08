208384e1ccfb7e79a6b6fedbc40a49b40277c860
nguyenma <mai-anh.nguyen@web.de>
2015-07-08 19:36:02 +0200

    and again und startposition == Startanimation

--- Project_Gruppe1/Assets/CloseEyesState.cs
+++ Project_Gruppe1/Assets/CloseEyesState.cs
@@ -20,7 +20,7 @@ public class CloseEyesState : StateMachineBehaviour {
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 	//
 //	var eyesScript = GameObject.Find ("EyesCanvas").GetComponentInChildren<EyesAnimation> ();
-//	eyesScript.OpenEyes ();
+//	eyesScript.CloseEyes ();
 	}
 
 	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
