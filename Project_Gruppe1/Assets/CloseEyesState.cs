c4078886e2e1ca3a54a53659f5a742adf3e8901b
nguyenma <mai-anh.nguyen@web.de>
2015-07-08 19:19:25 +0200

    start animation
    Augen öffnen sich

--- Project_Gruppe1/Assets/CloseEyesState.cs
+++ Project_Gruppe1/Assets/CloseEyesState.cs
@@ -1,37 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
-using UnityStandardAssets.Characters.FirstPerson;
 
-public class Wakeup : StateMachineBehaviour{
-	
+public class CloseEyesState : StateMachineBehaviour {
+
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
 //		var eyesScript = GameObject.Find ("EyesCanvas").GetComponentInChildren<EyesAnimation> ();
-//		eyesScript.OpenEyes ();
-		//Player Control zurücksetzen, damit animation an richtiger stelle läuft
-		GameObject.FindGameObjectWithTag("Player").transform.localPosition =  new Vector3(0,0,0);
-		GameObject.FindGameObjectWithTag ("Player").transform.localRotation = Quaternion.identity;
-
-		//camera rotation zurücksetzen
-		Camera.main.transform.localRotation = Quaternion.identity;
-		animator.gameObject.GetComponentInChildren<FirstPersonController> ().m_MouseLook.ResetRotation ();
-
-
-
+//		eyesScript.CloseEyes ();
 	}
 
-
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-//	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	//
-
-//	}
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	var eyesScript = GameObject.Find ("EyesCanvas").GetComponentInChildren<EyesAnimation> ();
+	eyesScript.CloseEyes ();
+	}
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 	//
-	//}
+//	var eyesScript = GameObject.Find ("EyesCanvas").GetComponentInChildren<EyesAnimation> ();
+//	eyesScript.OpenEyes ();
+	}
 
 	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
 	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
@@ -42,5 +32,4 @@ public class Wakeup : StateMachineBehaviour{
 	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
 	//
 	//}
-	
 }
