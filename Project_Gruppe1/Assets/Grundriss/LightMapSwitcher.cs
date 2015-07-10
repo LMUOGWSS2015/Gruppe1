7ac098df3ad582606e571dbb5fc180dfd24f5d91
Jens <jensffb@gmail.com>
2015-07-10 21:13:40 +0200

    Lightmaps Fix

--- Project_Gruppe1/Assets/Grundriss/LightMapSwitcher.cs
+++ Project_Gruppe1/Assets/Grundriss/LightMapSwitcher.cs
@@ -4,11 +4,15 @@ using System.Linq;
 
 public class LightMapSwitcher : MonoBehaviour
 {
-	public Texture2D[] DayNear;
-	public Texture2D[] DayFar;
-	public Texture2D[] NightNear;
-	public Texture2D[] NightFar;
-	
+
+	private Object[] Day = new Texture2D[14];
+	private Object[] Night = new Texture2D[14];
+
+	private Texture2D[] DayNear = new Texture2D[7];
+	private Texture2D[] DayFar = new Texture2D[7];
+	private Texture2D[] NightNear = new Texture2D[7];
+	private Texture2D[] NightFar = new Texture2D[7];
+
 	private LightmapData[] dayLightMaps;
 	private LightmapData[] nightLightMaps;
 	
@@ -19,6 +23,36 @@ public class LightMapSwitcher : MonoBehaviour
 			Debug.Log("In order for LightMapSwitcher to work, the Near and Far LightMap lists must be of equal length");
 			return;
 		}
+
+		int x = 0, z = 0;
+		Day = Resources.LoadAll("Lightmaps_Day");
+		foreach (Object item in Day) {
+			if (item.name.Contains("_dir")){
+				DayNear[x] = item as Texture2D;
+				x++;
+			} else if (item.name.Contains("_light")){
+				DayFar[z] = item as Texture2D;
+				z++;
+			}
+		}
+
+		x = 0;
+		z = 0;
+		Night = Resources.LoadAll("Lightmaps_Night");
+		foreach (Object item in Night) {
+			if (item.name.Contains("_dir")){
+				NightNear[x] = item as Texture2D;
+				x++;
+			} else if (item.name.Contains("_light")){
+				NightFar[z] = item as Texture2D;
+				z++;
+			}
+		}
+
+		//DayNear = Resources.LoadAll<Texture2D>("Lightmaps_Day/Dir");
+		//DayFar = Resources.LoadAll<Texture2D>("Lightmaps_Day/Light");
+		//NightNear = Resources.LoadAll<Texture2D>("GLightmaps_Night/Dir");
+		//NightFar =  Resources.LoadAll<Texture2D>("Lightmaps_Night/Light");
 		
 		// Sort the Day and Night arrays in numerical order, so you can just blindly drag and drop them into the inspector
 		DayNear = DayNear.OrderBy(t2d => t2d.name, new NaturalSortComparer<string>()).ToArray();
